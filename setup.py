8311c81ddbd35cfad18a14fa7bf383f189b74699
zhoucx1119 <zhoucx1989@gmail.com>
2022-01-06 21:49:19 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='definexpfam',
+	version='1.0',
+	packages=['definexpfam'],
+	url='https://github.com/zhoucx1119/definexpfam',
+	license='',
+	author='Chenxi Zhou',
+	author_email='zhoucx1989@gmail.com',
+	description='Density estimation in finite-dimensional exponential families '
+)
